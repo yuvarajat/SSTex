1ae7aa5e19ade8ec2818b63f22c73d8ce0705bca
Yuvaraja T <beemyuva@gmail.com>
2023-12-25 15:40:29 +0530

    Revert "commit"
    This reverts commit 0d5d32b186717281932f8d8f8b004e7bfd5606b2.

--- index-script.js
+++ index-script.js
@@ -21,11 +21,5 @@ form.addEventListener('submit', function(e) {
     localStorage.setItem('invoiceNo', invoiceNo);
     localStorage.setItem('invoiceDate', invoiceDate);
 
-
-
     window.location.href = "bill.html";
 });
-
-function addParticulars(){
-    
-}
